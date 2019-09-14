36e261fdfb4b5b1389096c0eb1a5c938c978ab5a
Tucker <Ki6AFB@gmail.com>
2019-09-14 14:01:38 -0600

    9/14/19 progress

---
+++ CH_4_working_with_lists/buffet.py
@@ -0,0 +1,12 @@
+foods = ('Rice', 'noodles', 'beef', 'mash potatoes', 'soup')
+
+for food in foods:
+    print(food)
+print("\n")
+
+# test to make sure it breaks if i try to change tuple
+# foods[2] = 'salami'
+
+foods = ('brown Rice', 'noodles', 'Chicken', 'mash potatoes', 'soup')
+for food in foods:
+    print(food)
